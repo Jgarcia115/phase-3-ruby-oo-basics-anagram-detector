f23747f4a6c171300b4a6da1a10bdcb454f90cac
Jonathan <jono_95@yahoo.com>
2022-09-08 16:47:23 -0400

    commit tree 7f9c51167d313e1ab37ef78c5890bac14c1dc9e6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word, :list
+
+    def initialize(word)
+        @word = word
+        @compare = word.chars.sort
+    end
+
+    def match(list)
+        @newList = []
+        list.each do |word|
+            brokenDown = word.chars.sort
+            if brokenDown == @compare
+                @newList << word
+            end
+        end
+        return @newList
+    end
+
+
+end
+
+
+#initialize with start word,
+#take in list of words,
+#break down start word and list of words to chars, then sort
+#compare start word with each word in list broken down
+#when theres a match, sets word to a variable
